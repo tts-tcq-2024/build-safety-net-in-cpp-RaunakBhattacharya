ceb84325476ff875c8c25eb3eca0d802a7127ea3
RaunakBhattacharya <160215825+RaunakBhattacharya@users.noreply.github.com>
2024-07-06 18:10:48 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,32 +1,34 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-
 TEST(SoundexTest, HandlesEmptyString) {
     EXPECT_EQ(generateSoundex(""), "");
 }
 
-TEST(SoundexTest, HandlesSingleCharacter) {
-    EXPECT_EQ(generateSoundex("A"), "A000");
-}
-
-TEST(SoundexTest, HandlesMultipleCharacters) {
-    EXPECT_EQ(generateSoundex("ABCD"), "A123");
-}
-
-TEST(SoundexTest, HandlesSimilarSoundingCharacters) {
-    EXPECT_EQ(generateSoundex("Euler"), "E460");
-    EXPECT_EQ(generateSoundex("Ellery"), "E460");
-}
-
-TEST(SoundexTest, HandlesDissimilarSoundingCharacters) {
-    EXPECT_NE(generateSoundex("Euler"), generateSoundex("Gauss"));
+TEST(SoundexTest, ConvertsNamesToUpperCase) {
+    EXPECT_EQ(generateSoundex("name"), "N500");
 }
 
 TEST(SoundexTest, IgnoresNonAlphabeticCharacters) {
-    EXPECT_EQ(generateSoundex("A#B$C%D"), "A123");
+    EXPECT_EQ(generateSoundex("N123"), "N000");
 }
 
-TEST(SoundexTest, HandlesMixedCaseCharacters) {
-    EXPECT_EQ(generateSoundex("AbCd"), "A123");
+TEST(SoundexTest, LimitsLengthToFourCharacters) {
+    EXPECT_EQ(generateSoundex("Nemetz"), "N532");
+}
+
+TEST(SoundexTest, IgnoresVowelLikeLetters) {
+    EXPECT_EQ(generateSoundex("BaAeEiIoOuUhHyYcdl"), "B234");
+}
+
+TEST(SoundexTest, CombinesDuplicateEncodings) {
+    EXPECT_EQ(generateSoundex("Abfcgdt"), "A123");
+}
+
+TEST(SoundexTest, UppercasesFirstLetter) {
+    EXPECT_EQ(generateSoundex("abcd"), "A123");
+}
+
+TEST(SoundexTest, IgnoresCaseWhenEncodingConsonants) {
+    EXPECT_EQ(generateSoundex("BCDL"), generateSoundex("Bcdl"));
 }
