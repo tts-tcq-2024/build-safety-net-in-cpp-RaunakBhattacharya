9c367cb43d60fcd8877ce03591fc3ca62c6258fe
RaunakBhattacharya <160215825+RaunakBhattacharya@users.noreply.github.com>
2024-07-06 18:02:06 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -10,3 +10,23 @@ TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
 }
 
+TEST(SoundexTest, HandlesMultipleCharacters) {
+    EXPECT_EQ(generateSoundex("ABCD"), "A123");
+}
+
+TEST(SoundexTest, HandlesSimilarSoundingCharacters) {
+    EXPECT_EQ(generateSoundex("Euler"), "E460");
+    EXPECT_EQ(generateSoundex("Ellery"), "E460");
+}
+
+TEST(SoundexTest, HandlesDissimilarSoundingCharacters) {
+    EXPECT_NE(generateSoundex("Euler"), generateSoundex("Gauss"));
+}
+
+TEST(SoundexTest, IgnoresNonAlphabeticCharacters) {
+    EXPECT_EQ(generateSoundex("A#B$C%D"), "A123");
+}
+
+TEST(SoundexTest, HandlesMixedCaseCharacters) {
+    EXPECT_EQ(generateSoundex("AbCd"), "A123");
+}
