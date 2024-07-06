c9741a50edc57b20eaea000ddf98c22bc7b0b9f0
RaunakBhattacharya <160215825+RaunakBhattacharya@users.noreply.github.com>
2024-07-06 18:27:31 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,14 +1,18 @@
-#include "Soundex.h"
-#include <cctype>
-#include <string>
-
 std::string padWithZeros(const std::string& str) {
     auto zerosNeeded = 4 - str.length();
     return str + std::string(zerosNeeded, '0');
 }
 
-char getSoundexCode(char c) {
-    return soundexTable[toupper(c) - 'A'];
+std::string generateSoundexCode(const std::string& name, char& prevCode) {
+    std::string soundexCode;
+    for (size_t i = 1; i < name.length() && soundexCode.length() < 3; ++i) {
+        char code = getSoundexCode(name[i]);
+        if (code != '0' && code != prevCode) {
+            soundexCode += code;
+            prevCode = code;
+        }
+    }
+    return soundexCode;
 }
 
 std::string generateSoundex(const std::string& name) {
@@ -17,14 +21,7 @@ std::string generateSoundex(const std::string& name) {
     std::string soundex(1, toupper(name[0]));
     char prevCode = getSoundexCode(name[0]);
 
-    for (size_t i = 1; i < name.length(); ++i) {
-        char code = getSoundexCode(name[i]);
-        if (code != '0' && code != prevCode) {
-            soundex += code;
-            prevCode = code;
-        }
-        if (soundex.length() == 4) break;
-    }
+    soundex += generateSoundexCode(name, prevCode);
 
     return padWithZeros(soundex);
 }
