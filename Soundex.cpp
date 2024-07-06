3bb2b93fb3cef5ffdb3fcf6e065ce24576552ff5
RaunakBhattacharya <160215825+RaunakBhattacharya@users.noreply.github.com>
2024-07-06 18:07:51 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,31 +1,26 @@
 #include "Soundex.h"
 #include <cctype>
-#include <array>
+#include <string>
 
-std::array<char, 26> soundexTable = {
-    '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
-};
-
-char getSoundexCode(char c) {
-    return soundexTable[toupper(c) - 'A'];
+std::string generateSoundexCode(const std::string& name, char& prevCode) {
+    std::string soundexCode;
+    for (size_t i = 1; i < name.length() && soundexCode.length() < 3; ++i) {
+        char code = getSoundexCode(name[i]);
+        if (code != '0' && code != prevCode) {
+            soundexCode += code;
+            prevCode = code;
+        }
+    }
+    return soundexCode;
 }
 
 std::string generateSoundex(const std::string& name) {
     if (name.empty()) return "";
 
-    std::string soundex;
-    soundex.reserve(4);
-    soundex += toupper(name[0]);
-
+    std::string soundex(1, toupper(name[0]));
     char prevCode = getSoundexCode(name[0]);
 
-    for (size_t i = 1; i < name.length() && soundex.length() < 4; ++i) {
-        char code = getSoundexCode(name[i]);
-        if (code != '0' && code != prevCode) {
-            soundex += code;
-            prevCode = code;
-        }
-    }
+    soundex += generateSoundexCode(name, prevCode);
 
     while (soundex.length() < 4) {
         soundex += '0';
